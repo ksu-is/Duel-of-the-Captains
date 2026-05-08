8856f4d81e72a057e6ba89ba1312608cf81440e0
danhGIT3020 <dtran44@students.kennesaw.edu>
2026-05-08 00:54:27 -0400

    Update Player_class.py

--- Player_class.py
+++ Player_class.py
@@ -8,11 +8,15 @@ class Player:
   def turn(self):
     if self.action == "1" and self.loaded == True:
       print("You fired your cannons. Fire in the hole!!!")
-    elif self.action == "2":
+    if self.action == "2" and self.loaded == True:
+      print("Your cannons are already reloaded!")
+    elif self.action == "2" and self.loaded == False:
       print("You reloaded your cannons.")
       self.loaded = True
-    if self.action == "3" and self.bowsprit == True:
-      print("You evaded the enemy's move")
+    if self.action == "3" and self.bowsprit == False:
+      print("Without your bowsprit, you can't evade!")
+    elif self.action == "3" and self.bowsprit == True:
+      print("You evaded the enemy's move!")
     if self.action == "4" and self.bowsprit == True:
       print("You charged the enemy ship!")
     if self.action == "5":
